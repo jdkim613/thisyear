52ecae733d59fb8cc2beeb1947fcda5896771e2b
john-avanade <70712515+john-avanade@users.noreply.github.com>
2021-07-02 09:19:22 -0400

    Animation for section headers

--- js/action.js
+++ js/action.js
@@ -1,13 +1,54 @@
 gsap.registerPlugin(ScrollTrigger);
 tl = new TimelineMax ({});
 
-TweenMax.from('#page-title-trigger', .75, {y: '150%', ease: Power4.easeout})
-tl.staggerFrom('#highlighted-content-section .hidetext', 1, {scrollTrigger: {
+tl.staggerFrom('#title-container .hidetext', 0.75, {
+    scrollTrigger: {
+        trigger: '#title-container',
+        start: '30% center',
+        end: 'center center',
+        // markers: true,
+        // scrub: true,
+        toggleActions: 'play none reverse restart',
+    },
+    y: '150%', 
+    ease: Power4.easeout
+}, 0.25);
+
+tl.staggerFrom('#highlighted-content-section .hidetext', 1, {
+    scrollTrigger: {
     trigger: '#highlighted-content-section .hidetext',
-    start: 'top center',
-    markers: true,
-    toggleActions: 'play none none none',
-}, 
-y: '150%',
-ease: Power4.easeout
+    start: 'top 40%',
+    end: 'top 40%',
+    // markers: true,
+    scrub: 1,
+    toggleActions: 'play none reverse restart',
+    }, 
+    y: '150%',
+    ease: Power4.easeout
+}, 0.25);
+
+tl.staggerFrom('#more-content-section .hidetext', .75, {
+    scrollTrigger: {
+        trigger: '#more-content-section .hidetext',
+        start: 'top 40%',
+        end: 'top 40%',
+        // markers: true,
+        scrub: 1,
+        toggleActions: 'play none reverse restart',
+    },
+    y: '150%',
+    ease: Power4.easeout
+}, 0.25);
+
+tl.staggerFrom('#people-content-section .hidetext', .75, {
+    scrollTrigger: {
+        trigger: '#people-content-section .hidetext',
+        start: 'top 40%',
+        end: 'top 40%',
+        // markers: true,
+        scrub: 1,
+        toggleActions: 'play none reverse restart',
+    },
+    y: '150%',
+    ease: Power4.easeout
 }, 0.25);
